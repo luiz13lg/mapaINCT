351ff4508b75df88286544626ca285907b931020
Luiz G <luiz13lg13@gmail.com>
2020-05-05 21:00:24 -0300

    Enviando dados pela api, botão de atualizar marcadores.

--- api/index.js
+++ api/index.js
@@ -13,10 +13,10 @@ app.listen(3013,() => {                //servidor startado
 
 app.get('/', cors(), function (req, res) {          //resposta ao get
     console.log("acessado.");
-    // shell.exec("comando zabbix aqui!");
+    // let teste = shell.exec("ls &",{silent:true}).stdout;
     try{
         let data = fs.readFileSync('log.txt', 'utf-8');
-        // console.log(data);
+        data = converterJSON(data);
         res.write(data);
     }catch (err) {
         console.error("Erro ao obter o log das estações:");
@@ -24,3 +24,163 @@ app.get('/', cors(), function (req, res) {          //resposta ao get
     }
     res.end();
 });
+
+function converterJSON(string){
+	let primeiro_indice, segundo_indice = 0;
+	let json = "\{\"type\":\"FeatureCollection\",\"features\":["
+
+	primeiro_indice = string.indexOf('<tr><td>',primeiro_indice);
+	segundo_indice = string.indexOf('</td></tr>',segundo_indice);
+
+	let dados = string.slice(primeiro_indice, segundo_indice);
+	let primeiro_dado, segundo_dado = 0;
+	let estacao;
+	
+	json += "\{\"type\":\"Feature\",\"properties\":\{"
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	estacao = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Station": "${estacao}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Last": "${dado}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Size": "${dado}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Received": "${dado}",`;
+
+    primeiro_status = dado.indexOf(">");
+    segundo_status = dado.indexOf("</");
+    dado = dado.slice(primeiro_status+1,segundo_status);
+	json += `"Status": "${dado}"},`
+    
+	primeiro_status = dado.indexOf(">");
+	segundo_status = dado.indexOf("</");
+	dado = dado.slice(primeiro_status+1,segundo_status);
+	json += obterCoordenadasEstacao(estacao);
+	json += "},"
+
+	for(let auxString = 1; auxString < 17; auxString++){
+		primeiro_indice = string.indexOf('<tr><td>',(primeiro_indice+10));
+		segundo_indice = string.indexOf('</td></tr>',(segundo_indice+10));
+		dados = string.slice(primeiro_indice, segundo_indice);
+
+		primeiro_dado = segundo_dado = 0;
+		json += "\{\"type\":\"Feature\",\"properties\":\{"
+		primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+		segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+		estacao = dados.slice(primeiro_dado+4, segundo_dado);
+		json += `"Station": "${estacao}",`;
+
+		primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+		segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+		dado = dados.slice(primeiro_dado+4, segundo_dado);
+		json += `"Last": "${dado}",`;
+
+		primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+		segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+		dado = dados.slice(primeiro_dado+4, segundo_dado);
+		json += `"Size": "${dado}",`;
+
+		primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+		segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+		dado = dados.slice(primeiro_dado+4, segundo_dado);
+		json += `"Received": "${dado}",`;
+
+		primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+		segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+		dado = dados.slice(primeiro_dado+4, segundo_dado);
+		
+		primeiro_status = dado.indexOf(">");
+		segundo_status = dado.indexOf("</");
+		dado = dado.slice(primeiro_status+1,segundo_status);
+		
+		json += `"Status": "${dado}"},`;
+		json += obterCoordenadasEstacao(estacao);
+		json += "},"
+	}
+
+	primeiro_dado = segundo_dado = 0;
+	json += "\{\"type\":\"Feature\",\"properties\":\{"
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	estacao = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Station": "${estacao}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Last": "${dado}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Size": "${dado}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	json += `"Received": "${dado}",`;
+
+	primeiro_dado = dados.indexOf('<td>',(primeiro_dado+1));
+	segundo_dado = dados.indexOf('</td>',(segundo_dado+1));
+	dado = dados.slice(primeiro_dado+4, segundo_dado);
+	
+	primeiro_status = dado.indexOf(">");
+	segundo_status = dado.indexOf("</");
+	dado = dado.slice(primeiro_status+1,segundo_status);
+	
+	json += `"Status": "${dado}"},`;
+	json += obterCoordenadasEstacao(estacao);
+	json += "}"
+	json += "\]\}";
+	
+	return json;
+}
+
+function obterCoordenadasEstacao(estacao){
+	switch(estacao){
+		case "FRTZ":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":[-38.577635, -3.744554\]\}";
+		case "INCO":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-41.808178, -22.401163\]\}";
+		case "MAC3":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-48.311299, -10.199649\]\}";
+		case "PALM":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-51.119683, -30.073896\]\}";
+		case "POAL":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-51.407079, -22.122038\]\}";
+		case "PRU2":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-46.328114, -22.318548\]\}";
+		case "PRU4":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-51.408526, -22.120081\]\}";
+		case "SJCE":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-45.859743, -23.207528\]\}";
+		case "SJCU":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-45.956592, -23.210589\]\}";
+		case "SLMA":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-44.212290, -2.593464\]\}";
+		case "SPBO":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-48.432301, -22.852467\]\}";
+		case "STMC":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-47.523798, -18.724027\]\}";
+		case "STNT":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-35.196212, -5.840586\]\}";
+		case "STSN":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-55.544692, -11.829394\]\}";
+		case "STSH":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-54.344363, -24.846995\]\}";
+		case "UFBA":
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[-38.510659, -12.999826\]\}";
+		default:
+			return "\"geometry\":\{\"type\":\"Point\",\"coordinates\":\[0.0, 0.0\]\}";
+	}			
+}
--- main.js
+++ main.js
@@ -1,154 +1,163 @@
-// window.onload = init;
-
-function init(marcadores){
-    const map = new ol.Map({
-        // view layer target
-        view: new ol.View({
-            center: [-5602034.912633961, -1525141.1770111802],
-            zoom: 0,
-            // maxZoom: 0,
-            // extent: [-10449083.805621821, -4210178.782637534, -2296729.275207868, 941899.3487077651]
-        }),
-        layers: [
-            // new ol.layer.Tile({
-            //     source: new ol.source.BingMaps({
-            //         key: 'AiB6R3zkUT3jUgsmSQ2e9skbn0ck1YKHtuG9RwH4dEur8DW5nS6adQ7s9HZB6OM0',
-            //         imagerySet: 'AerialWithLabels'
-            //     })
-            // })
-            new ol.layer.Tile({
-                source: new ol.source.Stamen({
-                    layer: 'terrain'
-                })
+const map = new ol.Map({
+    // view layer target
+    view: new ol.View({
+        center: [-5602034.912633961, -1525141.1770111802],
+        zoom: 0,
+        // maxZoom: 0,
+        // extent: [-10449083.805621821, -4210178.782637534, -2296729.275207868, 941899.3487077651]
+    }),
+    layers: [
+        // new ol.layer.Tile({
+        //     source: new ol.source.BingMaps({
+        //         key: 'AiB6R3zkUT3jUgsmSQ2e9skbn0ck1YKHtuG9RwH4dEur8DW5nS6adQ7s9HZB6OM0',
+        //         imagerySet: 'AerialWithLabels'
+        //     })
+        // })
+        new ol.layer.Tile({
+            source: new ol.source.Stamen({
+                layer: 'terrain'
             })
-        ],
-        target: "js-map"
-    })
+        })
+    ],
+    target: "js-map"
+})
 
-    // console.log(marcadores);
+// console.log(marcadores);
 
-    //  estilo triangulo online
-    const shapeOffline = new ol.style.RegularShape({
-        fill: new ol.style.Stroke({
-            color:[222, 45, 18, 1]
-        }),
-        stroke: new ol.style.Stroke({
-            color:[30, 30, 31, 1],
-            width: 1.2
-        }),
-        points: 3,
-        radius: 8
-    });
-    const shapeOnline1h = new ol.style.RegularShape({
-        fill: new ol.style.Fill({
-            color:[17, 255, 0, 1]
-        }),
-        stroke: new ol.style.Stroke({
-            color:[30, 30, 31, 1],
-            width: 1.2
-        }),
-        points: 3,
-        radius: 8
-    });
-    const shapeOnline2h = new ol.style.RegularShape({
-        fill: new ol.style.Fill({
-            color:[27, 135, 52, 1]
-        }),
-        stroke: new ol.style.Stroke({
-            color:[30, 30, 31, 1],
-            width: 1.2
-        }),
-        points: 3,
-        radius: 8
-    });
-    const shapeOnline3h = new ol.style.RegularShape({
-        fill: new ol.style.Fill({
-            color:[41, 71, 48, 1]
-        }),
-        stroke: new ol.style.Stroke({
-            color:[30, 30, 31, 1],
-            width: 1.2
-        }),
-        points: 3,
-        radius: 8
-    });
+//  estilo triangulo online
+const shapeOffline = new ol.style.RegularShape({
+    fill: new ol.style.Stroke({
+        color:[222, 45, 18, 1]
+    }),
+    stroke: new ol.style.Stroke({
+        color:[30, 30, 31, 1],
+        width: 1.2
+    }),
+    points: 3,
+    radius: 8
+});
+const shapeOnline1h = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color:[17, 255, 0, 1]
+    }),
+    stroke: new ol.style.Stroke({
+        color:[30, 30, 31, 1],
+        width: 1.2
+    }),
+    points: 3,
+    radius: 8
+});
+const shapeOnline2h = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color:[27, 135, 52, 1]
+    }),
+    stroke: new ol.style.Stroke({
+        color:[30, 30, 31, 1],
+        width: 1.2
+    }),
+    points: 3,
+    radius: 8
+});
+const shapeOnline3h = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color:[41, 71, 48, 1]
+    }),
+    stroke: new ol.style.Stroke({
+        color:[30, 30, 31, 1],
+        width: 1.2
+    }),
+    points: 3,
+    radius: 8
+});
 
-    const estiloOnline1h = new ol.style.Style({
-        image: shapeOnline1h
-    })
-    const estiloOnline2h = new ol.style.Style({
-        image: shapeOnline2h
-    })
-    const estiloOnline3h = new ol.style.Style({
-        image: shapeOnline3h
-    })
+const estiloOnline1h = new ol.style.Style({
+    image: shapeOnline1h
+})
+const estiloOnline2h = new ol.style.Style({
+    image: shapeOnline2h
+})
+const estiloOnline3h = new ol.style.Style({
+    image: shapeOnline3h
+})
 
-    const estiloOffline = new ol.style.Style({
-        image: shapeOffline
-    })
+const estiloOffline = new ol.style.Style({
+    image: shapeOffline
+})
 
-    const estiloDaEstacao = function(feature){
-        let status = feature.get("Status");
+const estiloDaEstacao = function(feature){
+    let status = feature.get("Status");
 
-        if (status == "Online"){
-            let horaAtual = new Date().getHours();
-            let diaAtual = new Date().getDate();
+    if (status == "Online"){
+        let horaAtual = new Date().getHours();
+        let diaAtual = new Date().getDate();
 
-            if(feature.get("Received") != ""){
-                let horaString = feature.get("Received").split(" ");
-                let diaRecebido = parseInt(horaString[1]);
+        if(feature.get("Received") != ""){
+            let horaString = feature.get("Received").split(" ");
+            let diaRecebido = parseInt(horaString[1]);
 
-                if(diaAtual - diaRecebido == 0){
-                    if(horaString.length == 3){
-                        horaString = horaString[2].split(":");
-                        let horaRecebida = parseInt(horaString[0]);
+            if(diaAtual - diaRecebido == 0){
+                if(horaString.length == 3){
+                    horaString = horaString[2].split(":");
+                    let horaRecebida = parseInt(horaString[0]);
 
-                        (horaAtual - horaRecebida == 0 || horaAtual - horaRecebida == 1) ? feature.setStyle([estiloOnline1h]) :
-                            horaAtual - horaRecebida == 2 ? feature.setStyle([estiloOnline2h]) : feature.setStyle([estiloOnline3h]);
-                    }
+                    (horaAtual - horaRecebida == 0 || horaAtual - horaRecebida == 1) ? feature.setStyle([estiloOnline1h]) :
+                        horaAtual - horaRecebida == 2 ? feature.setStyle([estiloOnline2h]) : feature.setStyle([estiloOnline3h]);
                 }
-                else feature.setStyle([estiloOnline3h]);
-            } else feature.setStyle([estiloOnline3h]);
-        }
-        else feature.setStyle([estiloOffline])
+            }
+            else feature.setStyle([estiloOnline3h]);
+        } else feature.setStyle([estiloOnline3h]);
     }
+    else feature.setStyle([estiloOffline])
+}
 
-    marcadores = JSON.parse(marcadores); 
-    
-    const marcadoresLayer = new ol.layer.VectorImage({
+let marcadoresLayer = new ol.layer.VectorImage({
+    source: new ol.source.Vector({
+        url: 'http://localhost:3013',
+        format: new ol.format.GeoJSON(),
+    }),
+    visible: true,
+    style: estiloDaEstacao
+})
+map.addLayer(marcadoresLayer);
+
+const containerOverlay = document.querySelector('.containerOverlay');
+const station = document.getElementById('Station');
+const last = document.getElementById('Last');
+const size = document.getElementById('Size');
+const received = document.getElementById('Received');
+const status = document.getElementById('Status');
+
+const overlayLayer = new ol.Overlay({
+    element: containerOverlay
+});
+map.addOverlay(overlayLayer);
+
+map.on("click",(e)=>{
+    overlayLayer.setPosition(undefined);
+    map.forEachFeatureAtPixel(e.pixel, function(feature, layer){
+        let coordenadaClicada = e.coordinate;
+        overlayLayer.setPosition(coordenadaClicada);
+        
+        station.innerHTML = feature.get("Station");
+        last.innerHTML = feature.get("Last");
+        size.innerHTML = feature.get("Size");
+        received.innerHTML = feature.get("Received");
+        status.innerHTML = feature.get("Status");
+    })
+})
+
+function atualizarLayerMarcadores(marcadoresLayer){
+    let layerRemover = map.getLayers().array_[1];
+    map.removeLayer(layerRemover);
+
+    marcadoresLayer = new ol.layer.VectorImage({
         source: new ol.source.Vector({
-            url: './marcadores/marcadores.geojson',
+            url: 'http://localhost:3013',
             format: new ol.format.GeoJSON(),
         }),
         visible: true,
         style: estiloDaEstacao
     })
-
+    
     map.addLayer(marcadoresLayer);
-
-    const containerOverlay = document.querySelector('.containerOverlay');
-    const station = document.getElementById('Station');
-    const last = document.getElementById('Last');
-    const size = document.getElementById('Size');
-    const received = document.getElementById('Received');
-    const status = document.getElementById('Status');
-
-    const overlayLayer = new ol.Overlay({
-        element: containerOverlay
-    });
-    map.addOverlay(overlayLayer);
-
-    map.on("click",(e)=>{
-        overlayLayer.setPosition(undefined);
-        map.forEachFeatureAtPixel(e.pixel, function(feature, layer){
-            let coordenadaClicada = e.coordinate;
-            overlayLayer.setPosition(coordenadaClicada);
-            
-            station.innerHTML = feature.get("Station");
-            last.innerHTML = feature.get("Last");
-            size.innerHTML = feature.get("Size");
-            received.innerHTML = feature.get("Received");
-            status.innerHTML = feature.get("Status");
-        })
-    })
 }
